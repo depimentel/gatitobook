e305853a534b5bf5697e86f849b0573016846461
Delma Pimentel <delmap.silva@hotmail.com>
2021-12-20 00:25:03 -0300

    Add two-way data binding

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -10,7 +11,8 @@ import { LoginComponent } from './login/login.component';
   declarations: [HomeComponent, LoginComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule
   ],
   exports: [HomeComponent]
 })
